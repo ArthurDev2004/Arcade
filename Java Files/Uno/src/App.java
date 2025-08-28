b7aa847d71540ea6cff5637536c25fe8f5f11f05
FalseIokkon <149558736+FalseIokkon@users.noreply.github.com>
2025-08-27 17:08:14 -0700

    create card object

--- Java Files/Uno/src/App.java
+++ Java Files/Uno/src/App.java
@@ -1,15 +1,6 @@
 public class App {
 
 
-    public void card(){
-        /*example: {G|00} to represent a Green 0 card
-                   {X|W4} to represent a Wild +4 card
-                   {B|S!} to represent a Blue Skip Card
-                   {Y|R>} to represent a Yellow Reverse Card
-        */
-
-    }
-
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
---
+++ Java Files/Uno/src/card.java
@@ -0,0 +1,16 @@
+public class card {
+
+    String color; // (G)reen (B)lue (Y)ellow (R)ed (X)Black
+    int number;
+
+
+
+/*
+example: {G|00} to represent a Green 0 card
+         {X|W4} to represent a Wild +4 card
+         {B|S!} to represent a Blue Skip Card
+         {Y|R>} to represent a Yellow Reverse Card
+*/
+
+        
+}
