fa948e5ff38cd9fc818f90eb3a6cc94c145f282b
FalseIokkon <149558736+FalseIokkon@users.noreply.github.com>
2025-08-27 17:03:49 -0700

    Update App.java

--- Java Files/Uno/src/App.java
+++ Java Files/Uno/src/App.java
@@ -1,4 +1,14 @@
 public class App {
+
+
+    public void card(){
+        /*example: {G|00} to represent a Green 0 card
+                   {B|W4} to represent a Wild +4 card that is Black
+                   {}
+        */
+
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
