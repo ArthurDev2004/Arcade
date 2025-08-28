d417a8f323768b79b58b9f2d228fc314688641be
FalseIokkon <149558736+FalseIokkon@users.noreply.github.com>
2025-08-27 17:17:44 -0700

    added to Card

--- Java Files/Uno/src/App.java
+++ Java Files/Uno/src/App.java
@@ -1,7 +1,13 @@
+
+
 public class App {
 
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-    }
+
+        Card c1 = new Card("G", "01");
+        c1.printCard();
+
+    }   
 }
--- Java Files/Uno/src/card.java
+++ Java Files/Uno/src/card.java
@@ -1,16 +1,25 @@
-public class card {
+public class Card {
 
-    String color; // (G)reen (B)lue (Y)ellow (R)ed (X)Black
-    int number;
+    String color;   // (G)reen (B)lue (Y)ellow (R)ed (X)Black
+    String symbol;  //  sd
 
+    public Card(String color, String symbol){
+        this.color = color;
+        this.symbol = symbol;
+
+    }
+
+
+    public void printCard(){
+        System.out.printf("{%s|%s}", color, symbol);
+    }
 
 
 /*
-example: {G|00} to represent a Green 0 card
+example: {G|01} to represent a Green 1 card
          {X|W4} to represent a Wild +4 card
          {B|S!} to represent a Blue Skip Card
          {Y|R>} to represent a Yellow Reverse Card
 */
-
         
 }
